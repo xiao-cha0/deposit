b571e6a00021c6336142409a2ecd7ece08f90aaf
justin <1150216807@qq.com>
2020-03-02 10:25:38 +0800

    使用js实现了type切换

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li >收入</li>
+      <li :class="type ==='-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type ==='+' && 'selected'" @click="selectType('+')" >收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
   export default {
-    name: 'Types'
+    name: 'Types',
+    data(){
+      return{
+        type: '-' //'-'表示支出，'+'表示收入
+      }
+    },
+    methods: {
+      selectType(type){
+        if(type !=='-' && type !== '+'){
+          throw new Error('type is unknow');
+        }
+        this.type = type;
+      }
+    }
   };
 </script>
 
 <style lang="scss" scoped>
   .types{
-    background: black;
+    background: #ff930a;
     display: flex;
     text-align: center;
     font-size: 24px;
-    color:#ff930a;
+    color:black;
     border-radius: 5px;
     >li{
       width: 50%;
@@ -28,8 +41,8 @@
       justify-content: center;
       align-items: center;
       &.selected{
-        background: #ff930a;
-        color:black;
+        background:black ;
+        color:orange;
       }
     }
   }

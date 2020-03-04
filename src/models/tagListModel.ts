9babed05dd2b2b17034fcca7e767115ca8e605e0
justin <1150216807@qq.com>
2020-03-04 17:52:20 +0800

    解决id生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/components/lib/createId';
+
 const localStoragekeyName = 'tagList';
 type Tag = {
   id: string;
@@ -20,7 +22,8 @@ const tagListModel: TagListModel = {
   create(name){
     const names = this.data.map(item =>item.name);
     if(names.indexOf(name)>=0){return 'duplicated'}
-      this.data.push({id: name,name: name});
+    const id = createId().toString();
+      this.data.push({id,name: name});
       this.save();
       return 'success';
   },
